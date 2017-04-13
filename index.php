3e33771fb792fe85dc036fcc623145caf3b2df31
Mayrcon <mayrcon_marlon@hotmail.com>
2017-04-13 17:42:58 -0300

    Lista Artilharia

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
  * Date: 08/04/2017
  * Time: 08:08
  */
-include('templates/header.php'); ?>
+include('templates/header.php'); 
+include('templates/banco.php');
+?>
 
 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
@@ -48,72 +50,93 @@ include('templates/header.php'); ?>
     </a>
 </div>
 
-<h1>Escação dos Times</h1>
+<h1>Escalação dos Times</h1>
 
 <div class="row times">
 
     <div class="col-md-4">
         <div class="list-group">
             <a href="#" class="list-group-item active">
-                Cras justo odio
+                <?php 
+
+                    $result = mysqli_query($link, "SELECT `nome` FROM `time` WHERE `id` = 1");
+                    
+                    $time = mysqli_fetch_array($result);
+
+                    echo $time['nome']
+                ?>
             </a>
-            <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-            <a href="#" class="list-group-item">Morbi leo risus</a>
-            <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item">Vestibulum at eros</a>
-            <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-            <a href="#" class="list-group-item">Morbi leo risus</a>
-            <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item">Vestibulum at eros</a>
-            <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-            <a href="#" class="list-group-item">Morbi leo risus</a>
-            <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item">Vestibulum at eros</a>
+
+        <?php
+            $result = mysqli_query($link, "SELECT `time`.`nome` as nome_time, `jogador`.`nome`, `jogador`.`id_time` FROM `time` LEFT JOIN `jogador` ON `jogador`.`id_time` = `time`.`id`");
+            
+            while($jogador = mysqli_fetch_array($result)){
+                
+                if($jogador['id_time'] == 1){
+
+                echo '<a href="#" class="list-group-item">' . $jogador['nome'] . '</a>';
+                
+                }
+            }
+        ?>
+
         </div>
     </div>
     <div class="col-md-4">
         <div class="list-group">
             <a href="#" class="list-group-item active">
-                Cras justo odio
+               <?php 
+
+                    $result = mysqli_query($link, "SELECT `nome` FROM `time` WHERE `id` = 2");
+                    
+                    $time = mysqli_fetch_array($result);
+
+                    echo $time['nome']
+                ?>
             </a>
-            <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-            <a href="#" class="list-group-item">Morbi leo risus</a>
-            <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item">Vestibulum at eros</a>
-            <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-            <a href="#" class="list-group-item">Morbi leo risus</a>
-            <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item">Vestibulum at eros</a>
-            <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-            <a href="#" class="list-group-item">Morbi leo risus</a>
-            <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item">Vestibulum at eros</a>
+           <?php
+            $result = mysqli_query($link, "SELECT `time`.`nome` as nome_time, `jogador`.`nome`, `jogador`.`id_time` FROM `time` LEFT JOIN `jogador` ON `jogador`.`id_time` = `time`.`id`");
+            
+            while($jogador = mysqli_fetch_array($result)){
+                if($jogador['id_time'] == 2){
+
+                echo '<a href="#" class="list-group-item">' . $jogador['nome'] . '</a>';
+                
+                }
+            }
+        ?>
         </div>
     </div>
     <div class="col-md-4">
         <div class="list-group">
             <a href="#" class="list-group-item active">
-                Cras justo odio
+               <?php 
+
+                    $result = mysqli_query($link, "SELECT `nome` FROM `time` WHERE `id` = 3");
+                    
+                    $time = mysqli_fetch_array($result);
+
+                    echo $time['nome']
+                ?>
             </a>
-            <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-            <a href="#" class="list-group-item">Morbi leo risus</a>
-            <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item">Vestibulum at eros</a>
-            <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-            <a href="#" class="list-group-item">Morbi leo risus</a>
-            <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item">Vestibulum at eros</a>
-            <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-            <a href="#" class="list-group-item">Morbi leo risus</a>
-            <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item">Vestibulum at eros</a>
+            <?php
+            $result = mysqli_query($link, "SELECT `time`.`nome` as nome_time, `jogador`.`nome`, `jogador`.`id_time` FROM `time` LEFT JOIN `jogador` ON `jogador`.`id_time` = `time`.`id`");
+            
+            while($jogador = mysqli_fetch_array($result)){
+                if($jogador['id_time'] == 3){
+
+                echo '<a href="#" class="list-group-item">' . $jogador['nome'] . '</a>';
+                
+                }
+            }
+        ?>
         </div>
     </div>
 </div>
 
 <div class="row titulo">
     <div class="col-md-6">
-        <h1>Top-Score</h1>
+        <h1>Artilheiro</h1>
     </div>
     <div class="col-md-6">
         <h1>Jogos</h1>
@@ -123,47 +146,32 @@ include('templates/header.php'); ?>
     <div class="row">
         <div class="col-md-6">
             <ul class="list-group">
-                <li class="list-group-item">
-                    <span class="badge">20</span>
-                    Cras justo odio
-                </li>
-                <li class="list-group-item">
-                    <span class="badge">15</span>
-                    Cras justo odio
-                </li>
-                <li class="list-group-item">
-                    <span class="badge">14</span>
-                    Cras justo odio
-                </li>
-                <li class="list-group-item">
-                    <span class="badge">10</span>
-                    Cras justo odio
-                </li>
-                <li class="list-group-item">
-                    <span class="badge">8</span>
-                    Cras justo odio
-                </li>
-                <li class="list-group-item">
-                    <span class="badge">5</span>
-                    Cras justo odio
-                </li>
-                <li class="list-group-item">
-                    <span class="badge">4</span>
-                    Cras justo odio
-                </li>
-                <li class="list-group-item">
-                    <span class="badge">4</span>
-                    Cras justo odio
-                </li>
-                <li class="list-group-item">
-                    <span class="badge">2</span>
-                    Cras justo odio
-                </li>
+                <?php
+                $result = mysqli_query($link, "SELECT `jogador`.`nome`, `info_gol`.`jogador_id`, COUNT(`info_gol`.`jogador_id`) as gols FROM info_gol LEFT JOIN `jogador` ON `jogador`.`id` = `info_gol`.`jogador_id` GROUP BY `jogador_id` ORDER BY COUNT(`info_gol`.`jogador_id`) DESC");
+
+                while($artilheiro = mysqli_fetch_array($result)){
+                     echo   '<li class="list-group-item">
+                    <span class="badge">' . $artilheiro['gols'] . '</span>' . $artilheiro['nome'] . '</li>';
+                }
+
+                    ?>
+
+             
             </ul>
         </div>
         <div class="col-md-6">
             <div class="row">
-                <div class="col-md-4">
+               <div class="col-md-3">
+                    <ul class="list-group">
+                        <li class="list-group-item centro">27/05/2017</li>
+                        <li class="list-group-item centro">05/06/2017</li>
+                        <li class="list-group-item centro">10/06/2017</li>
+                        <li class="list-group-item centro">15/06/2017</li>
+                        <li class="list-group-item centro">20/06/2017</li>
+                        <li class="list-group-item centro">25/06/2017</li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
                     <ul class="list-group">
                         <li class="list-group-item centro">Time A</li>
                         <li class="list-group-item centro">Time B</li>
@@ -173,7 +181,7 @@ include('templates/header.php'); ?>
                         <li class="list-group-item centro">Time C</li>
                     </ul>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <ul class="list-group">
                         <li class="list-group-item centro">1 x 0</li>
                         <li class="list-group-item centro">2 x 0</li>
@@ -183,7 +191,7 @@ include('templates/header.php'); ?>
                         <li class="list-group-item centro">1 x 2</li>
                     </ul>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <ul class="list-group">
                         <li class="list-group-item centro">Time A</li>
                         <li class="list-group-item centro">Time B</li>
